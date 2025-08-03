777d876df10f64c07eb8102bca4619eb5c19b2b3
vickybhagat18 <vickybhagat424@gmail.com>
2025-08-03 19:30:07 +0530

    Fixed url_for index error

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Render-compatible
+    port = int(os.environ.get("PORT", 5000))  # Render-compatible
     app.run(host='0.0.0.0', port=port)
